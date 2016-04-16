8f280adfe428cc4ba0ad29ad1ac943c449683a23
Danielle Gellis <danisyellis@gmail.com>
2016-04-16 01:59:37 -0700

    add a second  haiku, with more words, each time the program is run

--- haiku_generator.js
+++ haiku_generator.js
@@ -36,3 +36,5 @@ function formatData(data){
 formatData(cmudictFile);
 
 console.log(haiku.createHaiku([[5],[7],[5]], objectOfWordandSyllables));
+console.log("")
+console.log(haiku.createHaiku([[[2],[3]],[[2],[1],[4]],[5]], objectOfWordandSyllables));
