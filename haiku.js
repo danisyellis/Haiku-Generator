6469055d78a0bd22377d90ce0705b6f970836824
Danielle Gellis <danisyellis@gmail.com>
2016-04-16 01:39:28 -0700

    It works

--- haiku.js
+++ haiku.js
@@ -1,5 +1,11 @@
-function createHaiku(structure){
-  console.log(structure)
+function createHaiku(structure, syllabelsArray){
+  var arrOfWords;
+  return structure.map(function(lines){
+    return lines.map(function(syls){
+      arrOfWords = syllabelsArray[syls];
+      return arrOfWords[Math.floor(Math.random() * arrOfWords.length)];
+    }).join(' ');
+  }).join('\n');
 }
 
 module.exports = {
--- haiku_generator.js
+++ haiku_generator.js
@@ -1,20 +1,38 @@
 var haiku = require('./haiku');
 var fs = require("fs");
 var cmudictFile = readCmudictFile('./cmudict.txt');
+var objectOfWordandSyllables = {};
 
 function readCmudictFile(file){
     return fs.readFileSync(file).toString();
 }
 
+
+//still need an object that has every word and its number of syllables so that it can be used in createHaiku
 function formatData(data){    
+	//makes every word+phonemes set its own line
    var lines = data.toString().split("\n"),
        lineSplit
+   var last = lines.pop();
+      //splits each line into the word (lineSplit[0]) and the phonemes (lineSplit[1])
    lines.forEach(function(line){    
-    lineSplit = line.split("  ");    
-    console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]); 
-  });   
+	    lineSplit = line.split("  ");    
+	    //console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]); 
+	    var theWord = lineSplit[0];
+	    //for each line, loops through an array of the phonemes to determine how many syllables the word has
+	    phonemesArray = lineSplit[1].split(" ")
+	    var numberOfSyllables = 0;
+	    for(var i=0; i<phonemesArray.length; i++) {
+	    	if(phonemesArray[i].match(/\d/)) {
+	    		numberOfSyllables ++
+	    	}
+	    }
+	    if(!objectOfWordandSyllables[numberOfSyllables]) {
+	    	objectOfWordandSyllables[numberOfSyllables] = [];
+	    }
+	    objectOfWordandSyllables[numberOfSyllables].push(theWord)
+	  });   
 }
-
 formatData(cmudictFile);
 
-console.log(haiku.createHaiku("5,7,5"));
+console.log(haiku.createHaiku([[5],[7],[5]], objectOfWordandSyllables));
